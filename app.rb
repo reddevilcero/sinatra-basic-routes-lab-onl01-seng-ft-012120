7256d260f3251ac5586dc496303fc4d3ad4ed86c
reddevilcero <reddevil_cero@hotmail.com>
2020-02-24 11:47:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello, World!'
+  end
+
+  get '/name' do
+    'My name is Pedro'
+  end
+
+  get '/hometown' do
+    'My hometown is Benidorm'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is none'
+  end
 end
